3d7f306a87849dd6d4a0453fc2d0a499da58497e
Charles Marsh <crmarsh@princeton.edu>
2014-01-05 12:31:15 +0530

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 equality
-========
+===
 
-Demonstration of a zero-error, O(1) information complexity algorithm for equality.
+Demonstration of a zero-error, O(1) information complexity algorithm for equality found in Interactive Information Complexity [[Braverman11](http://eccc.hpi-web.de/report/2011/123/)].
+
+# The Algorithm
+
+A basic outline of the procedure is as follows:
+
+1. Alice and Bob have private bit strings x and y, respectively.
+2. A non-singular n-by-n matrix A over the finite field F_2 is randomly sampled (with public randomness).
+3. For each row A_i in A:
+    1. Alice sends A_i • x to Bob.
+    2. If A_i • x != A_i • y, Bob returns `False`.
+4. Bob returns `True`.
+
+At termination, we have two possible outcomes:
+
+- If A_i • x != A_i • y for any i, then x != y.
+- If A_i • x == A_i • y for all i, then Ax = Ay. Further, as A is non-singular, this implies that x = y.
+
+# Running
+
+To run, initiate the test suite with `python equality.py`. Both known-equal strings and known-unequal strings will be tested.
+
+# Appendix
+
+The proof of the procedure's O(1) information complexity can be found in the [linked paper](http://eccc.hpi-web.de/report/2011/123/) mentioned above.
---
+++ equality.py
@@ -0,0 +1,94 @@
+"""
+    O(1) information test for equality of two bitstrings from Interactive Information Complexity [Braverman '11].
+"""
+
+import numpy as np
+from random import randint
+import unittest
+
+
+def is_invertible_F2(a):
+    """
+    Determine invertibility by Gaussian elimination.
+    Via http://stackoverflow.com/questions/16254654/test-if-matrix-is-invertible-over-finite-field.
+    """
+    a = np.array(a, dtype=np.bool_)
+    n = a.shape[0]
+    for i in range(n):
+        pivots = np.where(a[i:, i])[0]
+        if len(pivots) == 0:
+            return False
+
+        # swap pivot
+        piv = i + pivots[0]
+        row = a[piv, i:].copy()
+        a[piv, i:] = a[i, i:]
+        a[i, i:] = row
+
+        # eliminate
+        a[i + 1:, i:] -= a[i + 1:, i, None] * row[None, :]
+
+    return True
+
+
+def generate_A(n):
+    while True:
+        a = np.random.randint(0, high=2, size=(n, n))
+        if is_invertible_F2(a):
+            continue
+        return a
+
+
+class Player(object):
+
+    def __init__(self, data):
+        self.data = data
+        self.n = len(data)
+
+    def multiply(self, scale):
+        return np.dot(np.array(self.data), scale)
+
+    def isEqual(self, scale, b):
+        return self.multiply(scale) == b
+
+
+def equality(alice, bob):
+    A = generate_A(alice.n)
+
+    for row in A:
+        if not alice.isEqual(row, bob.multiply(row)):
+            return False
+
+    return True
+
+
+def random_bitstring(n):
+    return [randint(0, 1) for i in range(n)]
+
+
+class TestEquality(unittest.TestCase):
+
+    def test_equal(self):
+        for t in range(100):
+            bitstring = random_bitstring(100)
+            alice = Player(bitstring)
+            bob = Player(bitstring)
+            self.assertTrue(equality(alice, bob))
+
+    def test_inequality(self):
+        for t in range(100):
+            # assign Alice's data
+            bitstring = random_bitstring(100)
+            alice = Player(bitstring)
+
+            # ensure Bob has different data
+            bitstring = random_bitstring(100)
+            while alice.data == bitstring:
+                bitstring = random_bitstring(100)
+            else:
+                bob = Player(bitstring)
+
+            self.assertTrue(not equality(alice, bob))
+
+if __name__ == '__main__':
+    unittest.main()
